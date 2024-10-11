308062eb11438c2d06d46b04b00c0af55df2a06d
William Peru <wperu@paul-f4Ar5s11.clusters.42paris.fr>
2024-10-11 15:58:07 +0200

    test main

--- src/main/java/wperu/App.java
+++ src/main/java/wperu/App.java
@@ -5,6 +5,20 @@ package wperu;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(args.length);
+        System.out.println(args[0]);
+        String a = "console";
+        try {
+            if (args[0] == a)
+        {
+            System.out.println("Hello World!");
+        }
+        else if(args[0] == "gui")
+            System.out.println("gui");
+            
+        } catch (Exception e) {
+           System.out.println("truc");
+        }
+        
     }
 }
