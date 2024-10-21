4a55dbb3b7ec6bbe9529211256d7b8709cfb18b7
root <root@DESKTOP-3AHRVEM>
2024-10-21 10:07:58 +0200

    add Controller abstract

--- src/main/java/wperu/Controller/Controller.java
+++ src/main/java/wperu/Controller/Controller.java
@@ -1,7 +1,72 @@
 package wperu.Controller;
 
+import javax.swing.JFrame;
+
 public abstract class Controller {
    
+    protected Swingy swingy;
 
+    protected Controller(Swingy swingy)
+    {
+        this.swingy = swingy;
+    }
 
+    protected final boolean handleCommand(String input)
+    {
+        if(input == null)
+        {
+            return true;
+        }
+    if(input.equalsIgnoreCase("h") || input.equalsIgnoreCase("help"))
+    {
+        new Help(this).render();
+        return true;
+    }
+    else if (input.equalsIgnoreCase("home"))
+    {
+        swingy.useSaveController();
+        return true;
+    }
+    else if (input.equalsIgnoreCase("quit"))
+    {
+        swingy.quit();
+        return true;
+    }
+        else if (input.startsWith("set-display"))
+        {
+            String[] parts = input.split(" ");
+            if(parts.length == 2)
+            {
+                Mode mode = Mode.fromString(parts[1]);
+                if(mode != null)
+                {
+                    CurrentDisplay.setMode(mode);
+                    swingy.getUi().show();
+                    swingy.runOnce();
+                }
+                else
+                    System.out.println("Invalide set-display " + parts[1] + " , expedted console or gui");
+            }
+            else
+                System.out.println("Invalid set-display argument, missing argument");
+            return true;
+        }
+        return false;
+    }
+
+    abstract public void reset();
+
+    abstract public boolean handle(String event);
+
+    abstract public void run();
+
+    public final JFrame getFrame()
+    {
+        return swingy.getUi().getFrame();
+    }
+
+    public final UI getUi()
+    {
+        return swingy.getUi();
+    }
 }
